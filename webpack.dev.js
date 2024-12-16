cf0bf5efe7a883abe90a889ee9abafb961e49f67
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-12-15 22:25:57 -0400

    USE webpack-merge for different mode config files
    Create webpack config files for development and production mode
    As well as a common config file for shared configs to avoid repetition

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,30 +1,10 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
+module.exports = merge(common, {
+  mode: 'development',
   devtool: "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,6 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-};
+module.exports = merge(common, {
+  mode: 'production',
+});
